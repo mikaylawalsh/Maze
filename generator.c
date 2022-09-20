186413a0a043e2f4de96df7a142a24da2fb94b22
mikaylawalsh <mikayla_walsh@brown.edu>
2022-09-20 09:33:13 -0400

    ss

--- generator.c
+++ generator.c
@@ -76,8 +76,7 @@ void drunken_walk(int row, int col, int num_rows, int num_cols,
 
         if (get_neighbor(num_rows, num_cols, maze, r, directions[i]) == NULL) {
             r->dirs[directions[i]] = 1;
-            printf("%d\n", r->row);
-            printf("%d\n", r->col);
+            printf("%d\n", directions[i]);
         } else {
 
             struct maze_room *n = get_neighbor(num_rows, num_cols, maze, r, directions[i]);
